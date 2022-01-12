2cd91346b99d1eceb7312c4010532e0ec567200b
pralhad <51488927+pralhad677@users.noreply.github.com>
2022-01-11 21:09:03 -0800

    1

--- a.sh
+++ a.sh
@@ -4,6 +4,7 @@
 echo "my name is $name"
 echo "my name is $name and i live in kirtipur"
 jenkins githook scm pooling
+1
 # ifconfig ->eth0->inet->http://inetkoData:8081/github-webhook/
 #jasd
 exit 0
