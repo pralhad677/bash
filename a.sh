acc8f5749c72842084e72d7092c1377911c6c015
pralhad <51488927+pralhad677@users.noreply.github.com>
2022-01-11 21:07:44 -0800

    scm pooling

--- a.sh
+++ a.sh
@@ -3,6 +3,7 @@
  
 echo "my name is $name"
 echo "my name is $name and i live in kirtipur"
+jenkins githook scm pooling
 # ifconfig ->eth0->inet->http://inetkoData:8081/github-webhook/
 #jasd
 exit 0
